228126e6ca3b2e285a6891f226ba89271961d578
luanbalves <88348475+luanbalves@users.noreply.github.com>
2024-01-03 00:40:48 -0300

    show

--- SwiftStudentChallenge.swiftpm/GamingView.swift
+++ SwiftStudentChallenge.swiftpm/GamingView.swift
@@ -8,94 +8,115 @@
 import SwiftUI
 
 struct GamingView: View {
-    let words = ["Eu", "Estou", "Desenvolvendo"]
-        let numberOfColumns = [2, 5, 13]
-        var correctAnswers: [String] { words.map { String($0.map { _ in " " }) } }
-
-        @State private var currentWordIndex = 0
-        @State private var feedbackMessage: String? = nil
-        @State private var showTextField = false
-        @State private var userAnswers: [String] = []
-
-        var body: some View {
-            VStack {
-                if currentWordIndex < words.count {
-                    Text(words[currentWordIndex])
-                        .font(.title)
-                        .padding()
-                        .opacity(showTextField ? 0 : 1)
-                }
-
-                if showTextField {
-                    VStack {
-                        ForEach(0..<numberOfColumns.count, id: \.self) { colIndex in
-                            HStack {
-                                ForEach(0..<numberOfColumns[colIndex], id: \.self) { rowIndex in
-                                    let index = rowIndex + colIndex * numberOfColumns.max()!
-
-                                    if index < userAnswers.count {
-                                        TextView(bindingText: $userAnswers[index], onEditingChanged: { _ in })
-                                            .multilineTextAlignment(.center)
-                                            .frame(width: 30, height: 30)
-                                            .overlay(
-                                                RoundedRectangle(cornerRadius: 10)
-                                                    .stroke(Color.black, lineWidth: 1)
-                                            )
-                                            .keyboardType(.alphabet)
-                                            .onReceive(NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification)) { _ in
-                                                // Garante que apenas uma letra é permitida
-                                                if userAnswers[index].count > 1 {
-                                                    userAnswers[index] = String(userAnswers[index].prefix(1))
-                                                }
+    let words = ["EU", "ESTOU", "DESENVOLVENDO"]
+    let numberOfColumns = [2, 5, 13]
+    var correctAnswers: [String] { words.map { String($0.map { _ in " " }) } }
+    
+    @State private var currentWordIndex = 0
+    @State private var feedbackMessage: String? = nil
+    @State private var showTextField = false
+    @State private var userAnswers: [String] = []
+    
+    var body: some View {
+        VStack {
+            if currentWordIndex < words.count {
+                Text(words[currentWordIndex])
+                    .font(.title)
+                    .padding()
+                    .opacity(showTextField ? 0 : 1)
+            }
+            
+            if showTextField {
+                VStack {
+                    ForEach(0..<numberOfColumns.count, id: \.self) { colIndex in
+                        HStack {
+                            ForEach(0..<numberOfColumns[colIndex], id: \.self) { rowIndex in
+                                let index = rowIndex + colIndex * numberOfColumns.max()!
+                                
+                                if index < userAnswers.count {
+                                    TextView(bindingText: $userAnswers[index], onEditingChanged: { _ in })
+                                        .multilineTextAlignment(.center)
+                                        .frame(width: 30, height: 30)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(Color.black, lineWidth: 1)
+                                        )
+                                        .keyboardType(.alphabet)
+                                        .onReceive(NotificationCenter.default.publisher(for: UITextView.textDidChangeNotification)) { _ in
+                                            // Garante que apenas uma letra é permitida
+                                            if userAnswers[index].count > 1 {
+                                                userAnswers[index] = String(userAnswers[index].prefix(1))
                                             }
-                                    }
+                                        }
                                 }
                             }
                         }
                     }
-                    .padding()
-                }
-
-                Button("Verificar") {
-                    checkAnswer()
                 }
                 .padding()
-                .disabled(currentWordIndex == words.count || showTextField)
-
-                if let feedbackMessage = feedbackMessage {
-                    Text(feedbackMessage)
-                        .foregroundColor(feedbackMessage == "Correto!" ? .green : .red)
-                        .padding()
-                }
             }
-            .onAppear {
-                userAnswers = Array(repeating: "", count: numberOfColumns.max()! * words.count)
-                startTimer()
+            
+            Button("Verificar") {
+                checkAnswer()
             }
-        }
+            .padding()
+            .disabled(!allFieldsFilled() || !showTextField)
 
-        func startTimer() {
-            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-                if self.currentWordIndex < self.words.count - 1 {
-                    self.currentWordIndex += 1
-                } else {
-                    timer.invalidate()
-                    self.showTextField = true
-                }
+            if let feedbackMessage = feedbackMessage {
+                Text(feedbackMessage)
+                    .foregroundColor(feedbackMessage == "Correto!" ? .green : .red)
+                    .padding()
             }
         }
-
-        func checkAnswer() {
-            let correctAnswersCombined = correctAnswers.joined().lowercased()
-            let userAnswersCombined = userAnswers.joined().lowercased()
-
-            if userAnswersCombined == correctAnswersCombined {
-                feedbackMessage = "Correto!"
+        .onAppear {
+            userAnswers = Array(repeating: "", count: numberOfColumns.max()! * words.count)
+            startTimer()
+        }
+    }
+    
+    private func allFieldsFilled() -> Bool {
+        for wordIndex in 0..<words.count {
+            let startIndex = wordIndex * numberOfColumns.max()!
+            let endIndex = startIndex + numberOfColumns[wordIndex]
+            if endIndex <= userAnswers.count && !userAnswers[startIndex..<endIndex].allSatisfy({ !$0.isEmpty }) {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if self.currentWordIndex < self.words.count - 1 {
+                self.currentWordIndex += 1
             } else {
-                feedbackMessage = "Errado!"
+                timer.invalidate()
+                self.showTextField = true
             }
         }
     }
+    
+    private func areAnswersCorrect() -> Bool {
+        for wordIndex in 0..<words.count {
+            let startIndex = wordIndex * numberOfColumns.max()!
+            let endIndex = startIndex + numberOfColumns[wordIndex]
+            let userAnswerForWord = userAnswers[startIndex..<endIndex].joined().lowercased()
+            let correctAnswerForWord = correctAnswers[wordIndex].lowercased()
+
+            if userAnswerForWord != correctAnswerForWord {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    private func checkAnswer() {
+        let isCorrect = areAnswersCorrect()
+        feedbackMessage = isCorrect ? "Correto!" : "Errado!"
+    }
+
+}
 
 class Coordinator: NSObject, UITextFieldDelegate {
     var parent: TextView
@@ -163,7 +184,6 @@ struct TextView: UIViewRepresentable {
     }
 }
 
-
-    #Preview {
+#Preview {
     GamingView()
 }
